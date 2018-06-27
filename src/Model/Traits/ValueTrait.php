2e5161c5c41b6becd25e9ecd695daa0ad7589f16
Sam <samuele.salvatico@cws.it>
2018-06-27 15:16:40 +0200

    Reprocessed

--- src/Model/Traits/ValueTrait.php
+++ src/Model/Traits/ValueTrait.php
@@ -64,10 +64,22 @@ trait ValueTrait
                 break;
             case 'array':
                 $parts = [];
-                foreach ($value as $item) {
-                    $parts[] = $this->renderTyped($item);
+                $isAssociative = ($this->isAssociative($value) && $this->hasStringKeys($value));
+                if ($isAssociative) {
+                    $tempValue = '['.PHP_EOL;
+                    foreach ($value as $key => $item) {
+                        $tempValue .= "\t\t'" . $key . "' => " . $this->renderTyped($item) . ',' . PHP_EOL;
+                        $parts[$key] = $this->renderTyped($item);
+                    }
+                    $tempValue = rtrim($tempValue, ",");
+                    $value = $tempValue . "\t]";
+
+                } else {
+                    foreach ($value as $item) {
+                        $parts[] = $this->renderTyped($item);
+                    }
+                    $value = '['.PHP_EOL."\t\t" . implode(', '.PHP_EOL."\t\t", $parts) . PHP_EOL."\t]";
                 }
-                $value = '[' . implode(', ', $parts) . ']';
 
                 break;
             default:
@@ -75,5 +87,21 @@ trait ValueTrait
         }
 
         return $value;
+    }
+
+    private function isAssociative(array $arr)
+    {
+        if (array() === $arr) return false;
+        return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
+    private function hasStringKeys(array $arr)
+    {
+        if (array() === $arr) return false;
+        if (!count($arr)) return false;
+        $hasKey = true;
+        foreach ($arr as $key => $value) {
+            return !is_numeric($key);
+        }
     }
 }
